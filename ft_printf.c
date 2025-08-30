ffa9985aba5fd192247909b9093012d7cc2b2623
Vasco Castro <vascocastro2001@gmail.com>
2025-08-30 04:10:22 +0100

    Fix portability
    va_list must be passed as a pointer to function the right way;

--- ft_printf.c
+++ ft_printf.c
@@ -24,25 +24,25 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-static int	printer(char format, va_list ap)
+static int	printer(char format, va_list *ap)
 {
 	int	counter;
 
 	counter = 0;
 	if (format == 'c')
-		counter = lprint_chr(va_arg(ap, int));
+		counter = lprint_chr(va_arg(*ap, int));
 	else if (format == 's')
-		counter = lprint_str(va_arg(ap, char *));
+		counter = lprint_str(va_arg(*ap, char *));
 	else if (format == 'p')
-		counter = lprint_ptr(va_arg(ap, void *));
+		counter = lprint_ptr(va_arg(*ap, void *));
 	else if (format == 'd' || format == 'i')
-		counter = lprint_dig((long)va_arg(ap, int), DECIMAL);
+		counter = lprint_dig((long)va_arg(*ap, int), DECIMAL);
 	else if (format == 'u')
-		counter = lprint_dig(va_arg(ap, unsigned int), DECIMAL);
+		counter = lprint_dig(va_arg(*ap, unsigned int), DECIMAL);
 	else if (format == 'x')
-		counter = lprint_hex((long)va_arg(ap, unsigned int), HEXA_LOW);
+		counter = lprint_hex((long)va_arg(*ap, unsigned int), HEXA_LOW);
 	else if (format == 'X')
-		counter = lprint_hex((long)va_arg(ap, unsigned int), HEXA_UP);
+		counter = lprint_hex((long)va_arg(*ap, unsigned int), HEXA_UP);
 	else if (format == '%')
 		counter = lprint_chr(format);
 	return (counter);
@@ -62,7 +62,7 @@ int	ft_printf(const char *str, ...)
 	while (str[i])
 	{
 		if (str[i] == '%')
-			counter += printer(str[++i], ap);
+			counter += printer(str[++i], &ap);
 		else
 			counter += lprint_chr(str[i]);
 		i++;
